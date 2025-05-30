7977632e0d3a5c3796caf25d909f1e56393b748c
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 11:40:46 -0300

    Feat: Adiciona atividade 13

---
+++ scripts/atividade13.js
@@ -0,0 +1,12 @@
+//🔹 13 – Relatório de férias: Primeira semana
+let diasFaltas = [
+    "Carlos – Segunda",
+    "Carlos – Terça",
+    "Carlos – Quarta",
+    "Carlos – Quinta",
+    "Carlos – Sexta",
+    "Carlos – Segunda (semana 2)",
+    "Carlos – Terça (semana 2)",
+    "Carlos – Quarta (semana 2)"
+  ];
+console.log("Primeira semana ferias", diasFaltas.slice(0, 5)); 
