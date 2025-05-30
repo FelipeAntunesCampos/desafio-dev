03e07ade9f69b2e72be1157ecea8559915894447
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 11:53:51 -0300

    Feat: Adiciona atividade 19

---
+++ scripts/atividade19.js
@@ -0,0 +1,13 @@
+//🔹 19 – Códigos de produtos formatados
+let codigos = [
+    "abc123",
+    "def456",
+    "ghi789",
+    "jkl012",
+    "mno345",
+    "pqr678",
+    "stu901",
+    "vwx234"
+  ];
+let codigosEmCaps = codigos.map(codigosAleatorios => codigosAleatorios.toUpperCase());
+console.log("Codigos em letras maiúsculas:", codigosEmCaps);
