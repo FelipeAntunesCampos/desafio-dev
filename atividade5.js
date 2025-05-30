7091d665ffed02dc642a61de2c5cade124c2af99
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 11:05:17 -0300

    Feat: Adiciona atividade 5

---
+++ atividade5.js
@@ -0,0 +1,13 @@
+//🔹 5 – Erro de importação
+let importados = [
+    "carregador portátil",
+    "cabo HDMI",
+    "leitor biométrico",
+    "controle sem fio",
+    "webcam 4K"
+  ];
+// removendo o primeiro item da lista
+let itemRemovido = importados.shift();
+
+console.log("Depois da remoção do primeiro item da lista", itemRemovido);
+console.log("Item que foi removido: ", itemRemovido);  
