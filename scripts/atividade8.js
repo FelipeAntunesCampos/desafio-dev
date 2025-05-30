232511d21a5a1370a391051ab2e7be5737c5c90d
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 11:24:48 -0300

    Feat: Adiciona arquivo

--- scripts/atividade8.js
+++ scripts/atividade8.js
@@ -1,1 +1,13 @@
 //🔹 8 – Alunos transferidos
+let chamada = [
+    "Carlos",
+    "Fernanda",
+    "João",
+    "Mariana",
+    "Lucas",
+    "Camila"
+  ];
+// adicionando alunas na primeira posição do índice
+let alunosTransferidos = chamada.unshift("Beatriz", "Rafaela");
+
+console.log("Depois de adicionar os(a) alunos(a) transferidos", chamada);
