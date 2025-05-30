eaab35ef0656bfbc77fc942d291a8a85ae89bf2e
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 10:58:36 -0300

    Feat: Adiciona atividade 2

---
+++ atividade2.js
@@ -0,0 +1,14 @@
+//🔹 2 – Expansão de cardápio
+let cardapio = [
+    "pizza de calabresa",
+    "lasanha",
+    "sushi",
+    "salada Caesar",
+    "pastel",
+    "yakisoba",
+    "feijoada"
+  ];
+  
+  cardapio.push("hambúrguer artesanal", "wrap de frango") //adiciona os 2 itens ao final da lista
+  console.log(cardapio);
+  
