7e26adbcab3acc4f951faa88707703386993c703
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 11:01:49 -0300

    Feat: Adiciona atividade 3

---
+++ atividade3.js
@@ -0,0 +1,12 @@
+//🔹 3 – Último produto com defeito
+let producao = [
+    "mousepad",
+    "placa de vídeo",
+    "fonte de alimentação",
+    "HD externo",
+    "notebook gamer",
+    "roteador"
+  ];
+let itemRemovido = producao.pop("");
+console.log("Depois da remoção", producao);
+console.log("item com defeito removido: ",itemRemovido);
