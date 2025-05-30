117129f4ab1ecf1b7cb4f93d95739796fc31a7bf
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 14:49:46 -0300

    Feat: Adiciona atividade 26

---
+++ scripts/atividade26.js
@@ -0,0 +1,12 @@
+//🔹 26 – Histórico de navegação
+let historico = [
+  "login",
+  "painel",
+  "relatórios",
+  "detalhes do cliente",
+  "editar dados",
+  "configurações",
+  "logout"
+];
+let atualizacoesNavegacao = historico.reverse();
+console.log("O último acesso do usuário e os passos anteriores no sistema", atualizacoesNavegacao);
