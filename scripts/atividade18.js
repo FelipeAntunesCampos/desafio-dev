4b6018eb63e7b7deaf04033e409a355b83b67d32
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 11:50:58 -0300

    Feat: Adiciona atividade 18

---
+++ scripts/atividade18.js
@@ -0,0 +1,15 @@
+//🔹 18 – Catálogo unificado
+let hardware = [
+    "processador i7",
+    "placa-mãe",
+    "memória RAM 16GB",
+    "SSD 1TB"
+  ];
+  
+  let perifericos = [
+    "teclado gamer",
+    "mouse ótico",
+    "webcam HD",
+    "fone com microfone"
+  ];
+console.log("Listas de catálogo unificado:", hardware.concat(perifericos));  
