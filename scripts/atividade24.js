a7fa9c01e7b034a546bee8c8b5272fbb5f743f3c
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 14:45:10 -0300

    Feat: Adiciona atividade 24

---
+++ scripts/atividade24.js
@@ -0,0 +1,5 @@
+//🔹 24 – Ranking de vendas
+let vendas = [3500, 1800, 4200, 3900, 5000, 1200, 2900, 4700];
+// Ordenando os números de forma decrescente
+let numerosOrdenadosVendas = vendas.sort((a, b) => b - a);
+console.log("O ranking de vendas em ordem decrescente:", vendas);
