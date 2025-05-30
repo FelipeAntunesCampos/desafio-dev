14964e7484e6567ffe6965d4cdf354da0529ab66
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 11:50:06 -0300

    Feat: Adiciona atividade 17

---
+++ scripts/atividade17.js
@@ -0,0 +1,15 @@
+//🔹 17 – Atualização de lista de usuários
+let usuariosSistemaA = [
+    "aline.souza",
+    "maria.ferreira",
+    "carlos.lima",
+    "ana.martins"
+  ];
+  
+  let usuariosSistemaB = [
+    "joao.silva",
+    "paulo.pereira",
+    "fernanda.alves",
+    "marcelo.cardoso"
+  ];
+console.log("Listas de usuários combinadas:", usuariosSistemaA.concat(usuariosSistemaB));  
