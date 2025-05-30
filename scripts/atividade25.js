1c7e8c804d1d2b762f50c94a6b20a332a970efa2
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 14:47:57 -0300

    Feat: Adiciona atividade 25

---
+++ scripts/atividade25.js
@@ -0,0 +1,13 @@
+//🔹 25 – Últimas atualizações do sistema
+let atualizacoes = [
+  "Backup diário concluído",
+  "Servidor reiniciado",
+  "Nova versão implantada",
+  "Logs exportados",
+  "Firewall atualizado",
+  "Monitoramento ativado",
+  "Banco de dados otimizado",
+  "Script de segurança executado"
+];
+let atualizacoesInvertidas = atualizacoes.reverse();
+console.log("atualizações do sistema por ordem mais recentes", atualizacoesInvertidas);
