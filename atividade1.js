c7370f5b0928e8e406112c5c8592b660233d1b09
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 10:55:39 -0300

    Feat: Adiciona arquivo atividade

---
+++ atividade1.js
@@ -0,0 +1,14 @@
+//🔹 1 – Cadastro de novos produtos
+let promocoes = [
+    "teclado mecânico",
+    "mouse gamer",
+    "monitor ultrawide",
+    "cadeira ergonômica",
+    "webcam HD",
+    "fone bluetooth",
+    "notebook i5",
+    "hub USB"
+  ];
+
+promocoes.push("SSD 1TB", "mesa digitalizadora"); //Adiciona 2 itens ao final da lista
+console.log(promocoes); //Imprime o resultado
