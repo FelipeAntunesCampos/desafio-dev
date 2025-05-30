8df14a98d14531e9c4c7249914415de87abd09f8
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 14:37:00 -0300

    Feat: Adiciona atividade 23

---
+++ scripts/atividade23.js
@@ -0,0 +1,13 @@
+//🔹 23 – Organizar lista de candidatos
+let candidatos = [
+  "Marcos",
+  "Aline",
+  "Carla",
+  "João",
+  "Fernanda",
+  "Lucas",
+  "Bruno",
+  "Isabela"
+];
+candidatos.sort()
+console.log('A Ordem alfabética da lista é', candidatos);
