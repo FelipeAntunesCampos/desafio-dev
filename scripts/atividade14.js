00557ac0a185c82f67427f523a4860706f7bdbd6
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 11:42:04 -0300

    Feat: Adiciona atividade 14

---
+++ scripts/atividade14.js
@@ -0,0 +1,12 @@
+//🔹 14 – Destaques da semana no blog
+let artigosBlog = [
+    "Como usar Docker com Node.js",
+    "5 erros comuns em JavaScript",
+    "Introdução ao TypeScript",
+    "Clean Code na prática",
+    "Design Patterns úteis",
+    "Refatoração de código",
+    "Boas práticas de API REST",
+    "Segurança em aplicações web"
+  ];
+  console.log("Os 4 artigos mais recentes da semana", artigosBlog.slice(0, 4)); 
