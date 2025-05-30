e180aa054c62fcc9b77a947aae9a7791a82ec445
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 11:36:29 -0300

    Feat: Adiciona atividade 11

---
+++ scripts/atividade11.js
@@ -0,0 +1,10 @@
+//🔹 11 – Geração de frase
+let ingredientes = [
+    "farinha de trigo",
+    "fermento",
+    "leite",
+    "ovos",
+    "açúcar",
+    "manteiga"
+  ];
+  console.log("Ingredientes:", ingredientes.join(', '));  
