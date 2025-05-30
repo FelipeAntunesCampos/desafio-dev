9c42a1825e7aeae39c9f4c04c4196ac9d2826433
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 11:03:38 -0300

    Feat: Adiciona atividade 4

---
+++ atividade4.js
@@ -0,0 +1,7 @@
+//🔹 4 – Cancelamento de última inscrição
+let inscritos = [
+    "Pedro", "Larissa", "João", "Amanda", "Lucas", "Mariana"
+  ];
+let alunoRemovido = inscritos.pop("");
+console.log("Inscritos Depois da remoção", inscritos);
+console.log("Aluno removido: ",alunoRemovido);
